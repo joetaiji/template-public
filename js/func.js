aa1d5e2d306c281c2402d010bfe979d883c739ce
조정연 <joetaiji@plani.co.kr>
2024-12-24 14:25:12 +0900

    scroll추가

--- js/func.js
+++ js/func.js
@@ -269,6 +269,21 @@ function tabs(el, cont) {
     }
 }
 
+/*  ------------------------------------------------------------------------------------
+*   스크롤에 따라 콘텐츠 활성화                            scrollAC(".main .section")
+*   스크롤에 따라 콘텐츠 활성화, 콘텐츠 메뉴 활성화    		scrollAC(".tab_cont", ".depth4.scroll")
+------------------------------------------------------------------------------------- */
+function scrollAC(cont) {
+    const $cont = $(cont)	
+	$(window).on("load scroll", function() {    
+		$cont.each(function (i) {
+			contTop = $(this)[0].getBoundingClientRect().top
+			//콘텐츠 Active
+			contTop < $(window).height()*0.75 ? $(this).addClass(AC) : $(this).removeClass(AC)
+		})        
+		
+	})
+}
 
 /* -------------------------------------------------------------------------------------
 *   아코디언 - details로 수정( ※name속성을 W3C Validation에서 오류로 인식하기 때문에 data-name으로 그룹설정)
